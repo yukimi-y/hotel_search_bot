58bd04f94ee571f721d64c406bc956247b411dd6
yukimi.y <yyy.yukimi@gmail.com>
2021-04-10 10:19:58 +0900

    署名の検証実装

--- app/controllers/line_bot_controller.rb
+++ app/controllers/line_bot_controller.rb
@@ -3,6 +3,10 @@ class LineBotController < ApplicationController
 
   def callback
     body = request.body.read
+    signature = request.env['HTTP_X_LINE_SIGNATURE']
+    unless client.validate_signature(body, signature)
+      return head :bad_request
+    end
   end
 
   private
